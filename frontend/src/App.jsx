9dfc44d33578133f2336d5fecd927e47656ea9de
David Gómez Lozano <125882624+DavidGom1@users.noreply.github.com>
2026-01-18 15:48:49 +0100

    feat: Agrega estructura y estilos iniciales al frontend de React

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,76 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import NoteItem from './components/NoteItem'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [notes, setNotes] = useState([
+    { id: 1, text: "Nota de ejemplo 1", completed: false },
+    { id: 2, text: "Nota de ejemplo 2", completed: false },
+  ])
+
+  const [newNote, setNewNote] = useState("");
+
+  const addNote = (e) => {
+    e.preventDefault();
+    let newNoteText = newNote.trim();
+    if(newNoteText){
+      setNotes([
+        ...notes,
+        {
+          id: Date.now(),
+          text: newNote,
+          completed: false
+        }
+      ]);
+      setNewNote("");
+    }
+  }
+
+  const toogleNote = (id) => {
+    setNotes(
+      notes.map((note) =>
+        note.id === id ? { ...note, completed: !note.completed } : note
+      )
+    )
+  }
+
+  const deleteNote = (id) => {
+    setNotes(notes.filter((note) => note.id !== id));
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='app'>
+        <div className='container'>
+          <h1>Mis notas</h1>
+          <form onSubmit={addNote}>
+            <input type="text"
+              value={newNote}
+              onChange={(e) => setNewNote(e.target.value)}
+              className='note-input'
+            />
+            <button type='submit' className='add-button'>
+              Añadir nota
+            </button>
+          </form>
+          <div className='notes-list'>
+            {notes.length == 0 ? (
+              <p className='no-notes'>No se encontraton notas</p>
+            ) : (
+              notes.map((note) => (
+                <NoteItem
+                  key={note.id}
+                  note={note}
+                  onToggle={toogleNote}
+                  onDelete={deleteNote}
+                />
+              ))
+            )
+            }
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count += 10)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
